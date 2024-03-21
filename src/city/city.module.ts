eb8557e2a2549146b3957660d05b157c9e89452b
PedroMakengo <pedromakengomiguel@gmail.com>
2024-03-21 20:47:50 +0100

    Trabalhando no meu endpoint de city

--- src/city/city.module.ts
+++ src/city/city.module.ts
@@ -1,4 +1,11 @@
 import { Module } from '@nestjs/common';
+import { CityController } from './city.controller';
+import { CityService } from './city.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
-@Module({})
+@Module({
+  imports: [TypeOrmModule.forFeature([CityController])],
+  controllers: [CityController],
+  providers: [CityService],
+})
 export class CityModule {}
