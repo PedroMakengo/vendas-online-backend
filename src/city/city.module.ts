1b0523a2877dabffc86d16e0f98e1e3fa1d57897
PedroMakengo <pedromakengomiguel@gmail.com>
2024-03-21 21:34:37 +0100

    Adicionado cache module

--- src/city/city.module.ts
+++ src/city/city.module.ts
@@ -1,14 +1,19 @@
 import { Module } from '@nestjs/common';
-import { CacheModule } from '@nestjs/cache-manager';
 import { CityController } from './city.controller';
 import { CityService } from './city.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { CityEntity } from './entities/city.entity';
+import { CacheModule } from '../cache/cache.module';
+
+import { CacheModule as CacheModuleNest } from '@nestjs/cache-manager';
 
 @Module({
   imports: [
-    CacheModule.register({ ttl: 900000000 }),
+    CacheModuleNest.register({
+      ttl: 900000000,
+    }),
     TypeOrmModule.forFeature([CityEntity]),
+    CacheModule,
   ],
   controllers: [CityController],
   providers: [CityService],
